7f8c4b3a5c0f5e575055fd78ab06302e9b668cbb
N1CKO11 <N1CK125@hotmail.com>
2021-11-14 13:05:43 -0500

    Initial commit

---
+++ Assets/Scripts/Gamemanagerscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanagerscript : MonoBehaviour
+{
+    public GameObject pausemenu;
+    public void pausebutton()
+    {
+        pausemenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void playbutton()
+    {
+        pausemenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
